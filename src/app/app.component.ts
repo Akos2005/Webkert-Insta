657bf7dfaa133acfa0921e81fed68e06109f43bf
Akos <agardika92@gmail.com>
2025-04-13 22:30:05 +0200

    warning javitasa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import {  RouterOutlet } from '@angular/router';
 import { MenuComponent } from './shared/menu/menu.component';
-import { MessageComponent } from './pages/message/message.component';
+
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet,
-    MenuComponent,
-    MessageComponent
+    MenuComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
